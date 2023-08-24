828b1a07cd026f13f50b9ffe20f89093b3c30a7c
TimLaine <timothee.laine@gmail.com>
2023-08-24 15:57:53 +0200

    Finished display for Admin page

--- admin.php
+++ admin.php
@@ -16,6 +16,84 @@ onlinespaceChange(500, 1);
     <link rel="stylesheet" href="CSS/styleadmin.css">
 </head>
 <body>
-    
+    <section id="layout">
+                <div id="title">
+                    <h1>Update Pricing</h1>
+                </div>
+                <div id="cards">
+                    <?php 
+                    foreach($landing as $pricing){
+                        $price = $pricing["prix"] * (1-($pricing["reduction"]/100));
+                        $onlineSpace = $pricing["onlinespace"];
+                        $onlineSpaceUnit = "MB";
+                        if($pricing["onlinespace"] >= 1000){
+                            $onlineSpaceUnit = "GB";
+                            $onlineSpace = $onlineSpace/1000;
+                        }                        
+                        $domain = $pricing["domain"];
+                        if($domain == 0){
+                            $domain = "Unlimited";
+                        }
+                        $support = ($pricing["support"]) ? "Yes" : "No";
+                        $HiddenFees = ($pricing["hidden_fees"]) ? "Yes" : "No";
+                        ?>
+                        <div class="card">                        
+                            <form action="" method="" enctype="multipart/form-data">
+                                <div class="options">
+                                    <label>
+                                        Name
+                                    </label>
+                                    <input type="text" name="name">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Price
+                                    </label>
+                                    <input type="number" step="any" name="price">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Sale
+                                    </label>
+                                    <input type="number" name="sale">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Bandwidth
+                                    </label>
+                                    <input type="number" step="any" name="bandwidth">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Onlinespace
+                                    </label>
+                                    <input type="number" step="any" name="onlinespace">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Support
+                                    </label>
+                                    <input type="number" step="any" name="support">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Domain
+                                    </label>
+                                    <input type="number" step="any" name="domain">
+                                </div>
+                                <div class="options">
+                                    <label>
+                                        Hidden Fees
+                                    </label>
+                                    <input type="number" step="any" name="hiddenfees">
+                                </div>
+                                <div class="button">
+                                    <input type="submit" name="submit" value="Update">
+                                </div>
+                            </form>
+                        </div>
+                    <?php } ?>
+                </div>
+    </section>
 </body>
 </html>
--- db-functions.php
+++ db-functions.php
@@ -8,7 +8,7 @@ $landingStatement = $db->prepare('SELECT * FROM pricing');
 $landingStatement->execute();
 $landing = $landingStatement->fetchAll();
 
-
+// This function allows for a change of name for the given option
 function nameChange($name,$id){
     $db = new PDO(
         'mysql:host=localhost;dbname=landing;charset=utf8',
@@ -18,6 +18,7 @@ function nameChange($name,$id){
     $dbStatement = $db->prepare("UPDATE pricing SET nom_pricing = '$name' WHERE id_pricing = $id");
     $dbStatement->execute();
 }
+// This one is to change price
 function priceChange($price,$id){
     $db = new PDO(
         'mysql:host=localhost;dbname=landing;charset=utf8',
@@ -27,6 +28,7 @@ function priceChange($price,$id){
     $dbStatement = $db->prepare("UPDATE pricing SET prix = '$price' WHERE id_pricing = $id");
     $dbStatement->execute();
 }
+// This one for the bandwidth
 function bandwidthChange($bandwidth,$id){
     $db = new PDO(
         'mysql:host=localhost;dbname=landing;charset=utf8',
@@ -36,6 +38,7 @@ function bandwidthChange($bandwidth,$id){
     $dbStatement = $db->prepare("UPDATE pricing SET bandwidth = '$bandwidth' WHERE id_pricing = $id");
     $dbStatement->execute();
 }
+// And this one for the onlinespace
 function onlinespaceChange($onlinespace,$id){
     $db = new PDO(
         'mysql:host=localhost;dbname=landing;charset=utf8',
